bebe99647b5e3898b1d1a55360db5a474270ae4a
SidV2 <sidharthv91@gmail.com>
2021-01-07 18:57:53 +0530

    Add files via upload

---
+++ reverseString.js
@@ -0,0 +1,14 @@
+
+function reverse(str) {
+    if(!str || str.length < 2 || typeof str !== 'string') {
+        const newArray = [];
+        for(var i = str.length - 1; i >= 0; i--) {
+          if(str[i] !== '') {
+            newArray.push(str[i]);
+          }
+        }
+        var reversedString = newArray.toString();
+        return reversedString.replace(/,/g, '');
+    }
+} 
+
