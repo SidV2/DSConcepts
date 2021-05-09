d571c8cb47845078cc075baa351bc2ef79c605e2
SidV2 <sidharthv91@gmail.com>
2021-05-09 15:54:54 +0530

    Create StackLinkedList.js

---
+++ StackLinkedList.js
@@ -0,0 +1,50 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+
+  peek() {
+    return this.top;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if(this.length === 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      const holdingPointer = this.top;
+      this.top = newNode;
+      this.top.next = holdingPointer;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if(!this.top) {
+      return null;
+    }
+    const leadingPointer = this.top.next;
+    this.top = leadingPointer;
+    this.length--;
+    return this;
+  }
+}
+
+const myStack = new Stack();
+myStack.push("Google");
+myStack.push("Udemy");
+myStack.push("Discord");
+myStack.pop();
+myStack.peek();
